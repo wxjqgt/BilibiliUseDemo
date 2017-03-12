769da5514e91aba789795f0c2c5d8bb8648b38f2
weibo <gt1254094162@163.com>
2017-03-12 19:29:50 +0800

    20170312commit-改用vitamio

---
+++ app/src/main/java/com/weibo/bilibiliusedemo/media/DanHandlerCallBack.java
@@ -0,0 +1,31 @@
+package com.weibo.bilibiliusedemo.media;
+
+import master.flame.danmaku.controller.DrawHandler.Callback;
+import master.flame.danmaku.danmaku.model.BaseDanmaku;
+import master.flame.danmaku.danmaku.model.DanmakuTimer;
+
+/**
+ * Created by weibo on 17-3-12.
+ */
+
+public class DanHandlerCallBack implements Callback{
+    @Override
+    public void prepared() {
+
+    }
+
+    @Override
+    public void updateTimer(DanmakuTimer timer) {
+
+    }
+
+    @Override
+    public void danmakuShown(BaseDanmaku danmaku) {
+
+    }
+
+    @Override
+    public void drawingFinished() {
+
+    }
+}
--- app/src/main/java/com/weibo/bilibiliusedemo/view/activitys/PlayActivity.java
+++ app/src/main/java/com/weibo/bilibiliusedemo/view/activitys/PlayActivity.java
@@ -1,25 +1,53 @@
 package com.weibo.bilibiliusedemo.view.activitys;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.FrameLayout;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
+import com.jakewharton.rxbinding2.view.RxView;
 import com.weibo.bilibiliusedemo.R;
+import com.weibo.bilibiliusedemo.media.DanHandlerCallBack;
 import com.weibo.bilibiliusedemo.view.BaseActivity;
 
+import java.util.Random;
+
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Consumer;
 import io.vov.vitamio.LibsChecker;
 import io.vov.vitamio.Vitamio;
-import io.vov.vitamio.provider.MediaStore;
+import io.vov.vitamio.widget.MediaController;
 import io.vov.vitamio.widget.VideoView;
+import master.flame.danmaku.danmaku.model.BaseDanmaku;
+import master.flame.danmaku.danmaku.model.IDanmakus;
+import master.flame.danmaku.danmaku.model.android.DanmakuContext;
+import master.flame.danmaku.danmaku.model.android.Danmakus;
+import master.flame.danmaku.danmaku.parser.BaseDanmakuParser;
+import master.flame.danmaku.ui.widget.DanmakuView;
 
 public class PlayActivity extends BaseActivity {
 
     private VideoView videoView;
     private String videoPath;
+    private boolean showDanmaku;
+    private DanmakuView danmakuView;
+    private DanmakuContext danmakuContext;
+    private EditText editText;
+    private Button send;
+    private LinearLayout operationLayout;
+
+    private BaseDanmakuParser parser = new BaseDanmakuParser() {
+        @Override
+        protected IDanmakus parse() {
+            return new Danmakus();
+        }
+    };
 
     @Override
     protected void onNewIntent(Intent intent) {
@@ -44,12 +72,62 @@ public class PlayActivity extends BaseActivity {
         }
         videoPath = intent.getStringExtra(MainActivity.VIDEO_PATH);
         videoView.setVideoPath(videoPath);
+        MediaController controller = new MediaController(this);
+        videoView.setMediaController(controller);
         videoView.start();
+
+        danmakuView.enableDanmakuDrawingCache(true);
+        danmakuView.setCallback(new DanHandlerCallBack() {
+            @Override
+            public void prepared() {
+                showDanmaku = true;
+                danmakuView.start();
+                generateSomeDanmaku();
+            }
+        });
+        danmakuContext = DanmakuContext.create();
+        danmakuView.prepare(parser, danmakuContext);
     }
 
     @Override
     protected void initView() {
         videoView = findView(R.id.buffer);
+        danmakuView = findView(R.id.danmaku_view);
+        operationLayout = findView(R.id.operation_layout);
+        send = findView(R.id.send);
+        editText = findView(R.id.edit_text);
+
+        RxView.clicks(danmakuView).subscribe(new Consumer<Object>() {
+            @Override
+            public void accept(@NonNull Object o) throws Exception {
+                if (operationLayout.getVisibility() == View.GONE) {
+                    operationLayout.setVisibility(View.VISIBLE);
+                } else {
+                    operationLayout.setVisibility(View.GONE);
+                    onWindowFocusChanged(true);
+                }
+            }
+        });
+
+        RxView.clicks(send).subscribe(new Consumer<Object>() {
+            @Override
+            public void accept(@NonNull Object o) throws Exception {
+                String content = editText.getText().toString();
+                if (!TextUtils.isEmpty(content)) {
+                    addDanmaku(content, true);
+                    editText.setText("");
+                }
+            }
+        });
+
+        getWindow().getDecorView().setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+            @Override
+            public void onSystemUiVisibilityChange(int visibility) {
+                if (visibility == View.SYSTEM_UI_FLAG_VISIBLE) {
+                    onWindowFocusChanged(true);
+                }
+            }
+        });
     }
 
     @Override
@@ -84,8 +162,81 @@ public class PlayActivity extends BaseActivity {
     }
 
     @Override
+    protected void onPause() {
+        super.onPause();
+        if (danmakuView != null && danmakuView.isPrepared()) {
+            danmakuView.pause();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (danmakuView != null && danmakuView.isPrepared() && danmakuView.isPaused()) {
+            danmakuView.resume();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        showDanmaku = false;
+        if (danmakuView != null) {
+            danmakuView.release();
+            danmakuView = null;
+        }
+    }
+
+    @Override
     protected int getLayoutId() {
         return R.layout.activity_play;
     }
 
+    /**
+     * 向弹幕View中添加一条弹幕
+     *
+     * @param content    弹幕的具体内容
+     * @param withBorder 弹幕是否有边框
+     */
+    private void addDanmaku(String content, boolean withBorder) {
+        BaseDanmaku danmaku = danmakuContext.mDanmakuFactory.createDanmaku(BaseDanmaku.TYPE_SCROLL_RL);
+        danmaku.text = content;
+        danmaku.padding = 5;
+        danmaku.textSize = sp2px(20);
+        danmaku.textColor = Color.WHITE;
+        danmaku.setTime(danmakuView.getCurrentTime());
+        if (withBorder) {
+            danmaku.borderColor = Color.GREEN;
+        }
+        danmakuView.addDanmaku(danmaku);
+    }
+
+    /**
+     * 随机生成一些弹幕内容以供测试
+     */
+    private void generateSomeDanmaku() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (showDanmaku) {
+                    int time = new Random().nextInt(300);
+                    String content = "" + time + time;
+                    addDanmaku(content, false);
+                    try {
+                        Thread.sleep(time);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    /**
+     * sp转px的方法。
+     */
+    public int sp2px(float spValue) {
+        final float fontScale = getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
 }
